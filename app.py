d3aecc702bbd6efaaaacd7613b81f0add919dd23
Amanda McNesby <mcnesbya@moravian.edu>
2025-11-20 13:27:15 -0500

    add changes for ec2

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ if __name__ == "__main__":
     init_db()
     add_example_data()
     print("Database started")
-    app.run(debug=True,port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
